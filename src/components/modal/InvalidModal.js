2e75ef0976202d03ed976a45afe6b938277e047d
Winstein Martins <sriwinstein@gmail.com>
2023-01-07 16:54:06 -0300

    Background click closes modal

--- src/components/modal/InvalidModal.js
+++ src/components/modal/InvalidModal.js
@@ -2,15 +2,24 @@ import styles from './InvalidModal.module.css';
 
 const InvalidModal = (props) => {
     return (
-        <div className={styles.modal} hidden={props.hidden}>
-            <div className={styles['modal-content']}>
-                <div className={styles['content-title']}>Invalid input</div>
-                <div className={styles['content-message']}>{props.message}</div>
-                <div className={styles['modal-controls']}>
-                    <button onClick={props.closeModal}>Okay</button>
+        <>
+            <div
+                className={styles.background}
+                onClick={props.closeModal}
+                hidden={props.hidden}
+            ></div>
+            <div className={styles.modal} hidden={props.hidden}>
+                <div className={styles['modal-content']}>
+                    <div className={styles['content-title']}>Invalid input</div>
+                    <div className={styles['content-message']}>
+                        {props.message}
+                    </div>
+                    <div className={styles['modal-controls']}>
+                        <button onClick={props.closeModal}>Okay</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
